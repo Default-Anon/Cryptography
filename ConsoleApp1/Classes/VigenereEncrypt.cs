69130bb0d753ba16e9f5872e0a4ae950e51e7591
Tuntematon <alert.prompt.1991@gmail.com>
2023-05-23 13:05:40 +0300

    crap

--- ConsoleApp1/Classes/VigenereEncrypt.cs
+++ ConsoleApp1/Classes/VigenereEncrypt.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class VigenereEncrypt
+public class VigenereEncrypt: IEncrypt<string>
 {
-
+    public const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public string encrypt(string input, string key)
+    {
+        string encrypted_String = "";
+        input = input.ToUpper();
+        key = key.ToUpper();
+        for (int i = 0, g = 0; i < input.Length && g < key.Length; i++, g++)
+        {
+            if (g + 1 == key.Length) { g = 0; }
+            int offsetModuleIndex = (ALPHABET.IndexOf(input[i]) + ALPHABET.IndexOf(key[g])) % ALPHABET.Length;
+            encrypted_String += ALPHABET[offsetModuleIndex];
+        }
+        Console.WriteLine(encrypted_String + '\n');
+        return encrypted_String;
+    }
+    public string decrypt(string input,string key) {
+        string decrypt_String = "";
+        input = input.ToUpper();
+        key = key.ToUpper();
+        for (int i = 0, g = 0; i < input.Length && g < key.Length; i++, g++)
+        {
+            if (g + 1 == key.Length) { g = 0; }
+            int log = ALPHABET.IndexOf(input[i]);
+            int log2 = ALPHABET.IndexOf(key[g]);
+            int offsetModuleIndex = (ALPHABET.IndexOf(input[i]) - ALPHABET.IndexOf(key[g])) % ALPHABET.Length; // This crap not worked
+            decrypt_String += ALPHABET[offsetModuleIndex];
+        }
+        Console.WriteLine(decrypt_String);
+        return decrypt_String;
+    }
 }
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,8 @@ namespace ConsoleApp1
         static string s_STR = "Trump graduated from the Wharton School with a bachelor's degree in 1968. He became president of his father's real estate business in 1971 and renamed it the Trump Organization. He expanded its operations to building and renovating skyscrapers, hotels, casinos, and golf courses and later started side ventures, mostly by licensing his name. From 2004 to 2015, he co-produced and hosted the reality television series The Apprentice. He and his businesses have been plaintiff or defendant in more than 4,000 state and federal legal actions, including six business bankruptcies.";
         static void Main(string[] args)
         {
-            var caesarEncrypt = new CaesarEncrypt();
-            caesarEncrypt.crackCaesarEncrypt(s_STR);
+            VigenereEncrypt vigenere = new VigenereEncrypt();
+            vigenere.decrypt(vigenere.encrypt(s_STR, "Secret"), "Secret");
             Console.ReadLine();
         }
     }
